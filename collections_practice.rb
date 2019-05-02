682fe86d3ebadca03d8cb7e52da26bbd5a99b51c
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-02 12:28:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(int)
+  int.sort!
+end
 
+def sort_array_desc(ints)
+  ints.sort! {|a,b| b <=> a}
+end 
+
+def sort_array_char_count(string)
+  string.sort! {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(num)
+  num.sort! {|x,y| x[1] <=> y[2]}
+end
+
+def reverse_array(nu)
+  nu.reverse
+end
+
+def kesha_maker(strings)
+  new_arr = []
+  strings.each do |n| n
+   i = 0 
+  while i < strings.length
+  block_given?
+  yield (strings[i])
+  i += 1 
+  end
+  array
+end
